4b2a24833cd5314f7df37f819b38f6c580af792a
blitz-flex <abashidzeikaa@gmail.com>
2024-05-28 22:48:36 +0400

    update SONGS

---
+++ Week 7 SQL/songs/7.sql
@@ -0,0 +1,7 @@
+-- write a SQL query that returns the average energy of songs that are by Drake.
+
+SELECT AVG(songs.energy)
+FROM songs
+JOIN artists
+on songs.artist_id = artists.id
+WHERE artists.name = 'Drake';
